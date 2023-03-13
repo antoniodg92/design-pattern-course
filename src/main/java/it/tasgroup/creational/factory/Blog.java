538da10b8b4ac0ee69beae285e7d76103bd59811
Anton <26896733+Antond0c@users.noreply.github.com>
2023-03-13 14:34:19 +0100

    Update Blog.java

--- src/main/java/it/tasgroup/creational/factory/Blog.java
+++ src/main/java/it/tasgroup/creational/factory/Blog.java
@@ -2,6 +2,7 @@ package it.tasgroup.creational.factory;
 
 public class Blog extends Website{
 
+/** documentation! */
     @Override
     protected void createWebsite() {
         pages.add(new PostPage());
